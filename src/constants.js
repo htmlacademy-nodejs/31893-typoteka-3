02c7bd93c3a93f6e209dcd182fa29d165bf12516
Aleksandr Frolov <sasha-frolov@yandex-team.ru>
2021-04-03 16:32:54 +0300

    module2-task3

--- src/constants.js
+++ src/constants.js
@@ -2,12 +2,25 @@
 
 const DEFAULT_COMMAND = `--help`;
 const USER_ARGV_INDEX = 2;
+
+const FILE_NAME = `mocks.json`;
+
 const ExitCode = {
   success: 0,
   error: 1
 };
 
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
 module.exports = {
+  FILE_NAME,
+  HttpCode,
   DEFAULT_COMMAND,
   USER_ARGV_INDEX,
   ExitCode
